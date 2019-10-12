9597269ef18ff65f2ea48c4e94267d6786f59c14
Thu <tanguyen@knox.edu>
2019-10-12 13:29:00 -0500

    Deleted take 2

--- main.py
+++
@@ -1,6 +0,0 @@
-#This is the original main.py
-<<<<<<< HEAD
-#This is main.py as it appears in Beryl/data_analysis branch
-=======
-# This is main.py as it appears in thu/data_analysis branch
->>>>>>> thu/data_analysis
